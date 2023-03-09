fa5c1e7165c9e3965ebe2d3fa9a2b5d71c28221b
Олег Спас <spas.oleh@gmail.com>
2023-03-09 20:41:32 +0100

    Add project files.

---
+++ Lab_3/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    public class Circle
+    {
+        private double _radius;
+        private double _centerX;
+        private double _centerY;
+        private double _pointX;
+        private double _pointY;
+        public Circle(double radius, double centerX, double centerY, double pointX, double pointY)
+        {
+            _radius = radius;
+            _centerX = centerX;
+            _centerY = centerY;
+            _pointX = pointX;
+            _pointY = pointY;
+        }
+        public double Area()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public double Circumference()
+        {
+            return 2 * Math.PI * _radius;
+        }
+        public string Check() {
+            double Eps = 0.000001;
+            double length = Math.Sqrt(Math.Pow((_pointX - _centerX), 2) + Math.Pow((_pointY - _centerY), 2));
+            if (Math.Abs(length - _radius) > Eps) {
+                if ((length - _radius) > Eps)
+                {
+
+                    return "Точка поза колом";
+                }
+                else {
+                    return "Точка в колі";
+                }
+            }
+            else 
+            {
+                return "Точка в крузі";
+            }
+        }
+    }
+}
---
+++ Lab_3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Circle c;
+            c=new Circle(Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)
+                , Convert.ToDouble(textBox3.Text), Convert.ToDouble(textBox4.Text));
+            label4.Text = (c.Area()).ToString();
+            label5.Text = (c.Circumference()).ToString();
+            label6.Text = c.Check();
+        }
+    }
+    
+}
